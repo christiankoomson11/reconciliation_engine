bebdebe3aa92b13ab4eac3204ec3aa4156a84393
Christian Koomson <your-github-email@example.com>
2026-08-04 23:59:00 +0100

    Add Spring Boot application and hello endpoint

---
+++ src/main/java/com/reconciliation/ReconciliationApiApplication.java
@@ -0,0 +1,12 @@
+package com.reconciliation;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReconciliationApiApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ReconciliationApiApplication.class, args);
+    }
+}
